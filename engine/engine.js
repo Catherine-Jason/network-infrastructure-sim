c34988174617b14b34cb8e229fb1c78fa0ac19c9
Catherine Jason <catherine.wunder89@icloud.com>
2026-04-23 21:25:32 -0400

    Add centralized game state for the simulator

--- engine/engine.js
+++ engine/engine.js
@@ -1,1 +1,12 @@
+// Centralized game state for the simulator
+
+export const State = {
+    devices: [],
+    connections: [],
+    packet: null,
+    selectedDeviceId: null,
+    selectedForLinkId: null,
+    pingSourceId: null,
+    pingTargetId: null
+};
 
