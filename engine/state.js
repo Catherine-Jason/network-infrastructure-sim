7a9b38ec049b98b9d1a90f80653e30705e6c6340
Catherine Jason <catherine.wunder89@icloud.com>
2026-04-23 22:15:44 -0400

    Add initial State object with default properties

--- engine/state.js
+++ engine/state.js
@@ -1,1 +1,13 @@
+export const State = {
+    devices: [],
+    connections: [],
+    packet: null,
 
+    selectedDeviceId: null,
+    selectedForLinkId: null,
+
+    pingSourceId: null,
+    pingTargetId: null,
+
+    mode: "select"
+};
