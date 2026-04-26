685afb225c3f55f9183ac2b6a779739eefc9af89
Catherine Jason <catherine.wunder89@icloud.com>
2026-04-26 16:14:12 -0400

    Update deviceModel.js

--- js/deviceModel.js
+++ js/deviceModel.js
@@ -1,1 +1,35 @@
-
+window.App = window.App || {};
+App.DeviceModel = (function() {
+    function createDevice(type, id, name, x, y) {
+        const device = {
+            id: id || App.Utils.generateId(),
+            type: type,
+            name: name || `${type}_${Math.floor(Math.random()*1000)}`,
+            x: x || 200, y: y || 200,
+            interfaces: []
+        };
+        if (type === 'router') {
+            device.interfaces = [
+                { name: 'Gig0/0', ip: '', mask: '', vlan: null, enabled: true },
+                { name: 'Gig0/1', ip: '', mask: '', vlan: null, enabled: true }
+            ];
+        } else if (type === 'switch') {
+            device.interfaces = [
+                { name: 'Fa0/1', ip: '', mask: '', vlan: 1, enabled: true },
+                { name: 'Fa0/2', ip: '', mask: '', vlan: 1, enabled: true },
+                { name: 'Fa0/3', ip: '', mask: '', vlan: 1, enabled: true },
+                { name: 'Fa0/4', ip: '', mask: '', vlan: 1, enabled: true }
+            ];
+        } else if (type === 'pc') {
+            device.interfaces = [
+                { name: 'eth0', ip: '', mask: '', vlan: null, enabled: true }
+            ];
+        } else if (type === 'cloud') {
+            device.interfaces = [
+                { name: 'Internet', ip: '', mask: '', vlan: null, enabled: true }
+            ];
+        }
+        return device;
+    }
+    return { createDevice };
+})();
