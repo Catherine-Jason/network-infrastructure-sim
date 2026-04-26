acc5330b9fe6dbd503adda4dda1c3a46546b0ebf
Catherine Jason <catherine.wunder89@icloud.com>
2026-04-26 16:18:13 -0400

    Update controlsUI.js

--- js/controlsUI.js
+++ js/controlsUI.js
@@ -1,1 +1,14 @@
-
+window.App = window.App || {};
+App.ControlsUI = (function() {
+    function addDevice(type) {
+        const pos = App.Utils.getRandomPosition();
+        App.Controller.addDevice(type, pos.x, pos.y);
+    }
+    function bind() {
+        document.getElementById('btnAddRouter').onclick = () => addDevice('router');
+        document.getElementById('btnAddSwitch').onclick = () => addDevice('switch');
+        document.getElementById('btnAddPC').onclick = () => addDevice('pc');
+        document.getElementById('btnAddCloud').onclick = () => addDevice('cloud');
+    }
+    return { bind };
+})();
