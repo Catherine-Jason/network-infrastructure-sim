ed625f1d5364a87f77fecb5475c7bfc8e88217da
Catherine Jason <catherine.wunder89@icloud.com>
2026-04-26 17:27:15 -0400

    Update controller.js

--- js/controller.js
+++ js/controller.js
@@ -1,12 +1,7 @@
 window.App = window.App || {};
 App.Controller = (function() {
-    function addDevice(type, x, y) {
-        App.Engine.addDevice(type, null, `${type}_${Date.now()}`, x, y);
-    }
-    function selectDevice(id) {
-        if (id) App.Engine.selectDevice(id);
-        else App.Engine.clearSelection();
-    }
+    function addDevice(type, x, y) { App.Engine.addDevice(type, null, `${type}_${Date.now()}`, x, y); }
+    function selectDevice(id) { if (id) App.Engine.selectDevice(id); else App.Engine.clearSelection(); }
     function createLink(fromId, toId) {
         const exists = App.Engine.getLinks().some(l => (l.from===fromId && l.to===toId) || (l.from===toId && l.to===fromId));
         if (!exists) App.Engine.addLink(fromId, toId);
