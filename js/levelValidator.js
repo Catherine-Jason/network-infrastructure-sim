4e91b158d7f0b0fa4ae8d922ee44e5873329f975
Catherine Jason <catherine.wunder89@icloud.com>
2026-04-26 17:21:13 -0400

    Update levelValidator.js

--- js/levelValidator.js
+++ js/levelValidator.js
@@ -8,15 +8,10 @@ App.LevelValidator = (function() {
         completedTasks.clear();
         App.EventBus.emit('tasksUpdated', getTaskStatus());
     }
-
     function getTaskStatus() {
         if (!currentLevel) return [];
-        return currentLevel.tasks.map(task => ({
-            ...task,
-            completed: completedTasks.has(task.id)
-        }));
+        return currentLevel.tasks.map(task => ({ ...task, completed: completedTasks.has(task.id) }));
     }
-
     function checkAllTasks() {
         if (!currentLevel) return;
         const allCompleted = currentLevel.tasks.every(task => completedTasks.has(task.id));
@@ -26,60 +21,45 @@ App.LevelValidator = (function() {
         }
         App.EventBus.emit('tasksUpdated', getTaskStatus());
     }
-
     function validateDeviceExists(deviceType, minCount = 1) {
-        const devices = App.Engine.getDevices().filter(d => d.type === deviceType);
-        return devices.length >= minCount;
+        return App.Engine.getDevices().filter(d => d.type === deviceType).length >= minCount;
     }
-
     function validateLinkExists(deviceId1, deviceId2) {
         const links = App.Engine.getLinks();
         return links.some(l => (l.from === deviceId1 && l.to === deviceId2) || (l.from === deviceId2 && l.to === deviceId1));
     }
-
     function validatePingSuccess(fromId, toId) {
-        const successes = App.PingEngine.getPingSuccesses();
-        return successes.some(p => p.from === fromId && p.to === toId);
+        return App.PingEngine.getPingSuccesses().some(p => p.from === fromId && p.to === toId);
     }
-
     function validateCommandExecuted(deviceId, command) {
         if (!window._commandHistory) window._commandHistory = [];
         return window._commandHistory.some(c => c.deviceId === deviceId && c.command === command);
     }
-
     function registerTaskCompletion(taskId) {
         if (completedTasks.has(taskId)) return;
         completedTasks.add(taskId);
         checkAllTasks();
     }
-
     function reevaluate() {
         if (!currentLevel) return;
         for (let task of currentLevel.tasks) {
             if (completedTasks.has(task.id)) continue;
             let isComplete = false;
-            if (task.type === 'deviceExists') {
-                isComplete = validateDeviceExists(task.deviceType, task.minCount || 1);
-            } else if (task.type === 'linkExists') {
-                isComplete = validateLinkExists(task.between[0], task.between[1]);
-            } else if (task.type === 'pingSuccess') {
-                isComplete = validatePingSuccess(task.from, task.to);
-            } else if (task.type === 'commandExecuted') {
-                isComplete = validateCommandExecuted(task.deviceId, task.command);
-            }
+            if (task.type === 'deviceExists') isComplete = validateDeviceExists(task.deviceType, task.minCount || 1);
+            else if (task.type === 'linkExists') isComplete = validateLinkExists(task.between[0], task.between[1]);
+            else if (task.type === 'pingSuccess') isComplete = validatePingSuccess(task.from, task.to);
+            else if (task.type === 'commandExecuted') isComplete = validateCommandExecuted(task.deviceId, task.command);
             if (isComplete) registerTaskCompletion(task.id);
         }
     }
-
-    App.EventBus.on('deviceAdded', () => reevaluate());
-    App.EventBus.on('linkCreated', () => reevaluate());
-    App.EventBus.on('deviceRemoved', () => reevaluate());
-    App.EventBus.on('pingExecuted', () => reevaluate());
+    App.EventBus.on('deviceAdded', reevaluate);
+    App.EventBus.on('linkCreated', reevaluate);
+    App.EventBus.on('deviceRemoved', reevaluate);
+    App.EventBus.on('pingExecuted', reevaluate);
     App.EventBus.on('commandExecuted', (data) => {
         if (!window._commandHistory) window._commandHistory = [];
         window._commandHistory.push(data);
         reevaluate();
     });
-
     return { setLevel, getTaskStatus, reevaluate };
 })();
