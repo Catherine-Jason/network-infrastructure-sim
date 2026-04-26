15890206da9d2c6c89f6e904e00c193128289219
Catherine Jason <catherine.wunder89@icloud.com>
2026-04-26 17:23:21 -0400

    Update inspectorUI.js

--- js/inspectorUI.js
+++ js/inspectorUI.js
@@ -7,13 +7,10 @@ App.InspectorUI = (function() {
         const dev = App.Engine.getDevices().find(d => d.id === id);
         if (!dev) return;
         let html = `<strong>${dev.name}</strong> (${dev.type})<br>ID: ${dev.id}<hr><strong>Interfaces:</strong><ul>`;
-        dev.interfaces.forEach(iface => {
-            html += `<li>${iface.name} – IP: ${iface.ip || 'unset'} / Mask: ${iface.mask || 'unset'}</li>`;
-        });
+        dev.interfaces.forEach(iface => { html += `<li>${iface.name} – IP: ${iface.ip || 'unset'}</li>`; });
         html += `</ul>`;
         container.innerHTML = html;
     }
     App.EventBus.on('deviceSelected', update);
-    App.EventBus.on('deviceUpdated', update);
     return { update };
 })();
