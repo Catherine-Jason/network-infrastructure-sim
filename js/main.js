b2c4384ee5319ed11f519e95b14ec945df616a9b
Catherine Jason <catherine.wunder89@icloud.com>
2026-04-26 17:46:26 -0400

    Delete js directory

--- js/main.js
+++
@@ -1,8 +0,0 @@
-window.App = window.App || {};
-(function() {
-    App.Progression.init(App.Levels);
-    App.CanvasRenderer.startLoop();
-    App.ControlsUI.bind();
-    document.getElementById('levelSelectButton').onclick = () => App.LevelSelectUI.openModal();
-    console.log('Simulator fully functional');
-})();
--- js/utils.js
+++
@@ -1,9 +0,0 @@
-window.App = window.App || {};
-App.Utils = (function() {
-    function generateId() { return Date.now() + '-' + Math.random().toString(36).substr(2, 8); }
-    function getRandomPosition(padding=100, maxX=1050, maxY=500) {
-        return { x: padding + Math.random()*(maxX-padding*2), y: padding + Math.random()*(maxY-padding*2) };
-    }
-    function showPopup(msg, type='info', duration=2500) { App.EventBus.emit('showPopup', { message: msg, type, duration }); }
-    return { generateId, getRandomPosition, showPopup };
-})();
