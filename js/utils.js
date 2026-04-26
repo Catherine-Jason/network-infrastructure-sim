7f5d9163e082015d2f7bf9627bcb8e6da26c940b
Catherine Jason <catherine.wunder89@icloud.com>
2026-04-26 16:11:34 -0400

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,1 +1,9 @@
-
+window.App = window.App || {};
+App.Utils = (function() {
+    function generateId() { return Date.now() + '-' + Math.random().toString(36).substr(2, 8); }
+    function getRandomPosition(padding=100, maxX=1100, maxY=500) {
+        return { x: padding + Math.random()*(maxX-padding*2), y: padding + Math.random()*(maxY-padding*2) };
+    }
+    function showPopup(msg, type='info', duration=2500) { App.EventBus.emit('showPopup', { message: msg, type, duration }); }
+    return { generateId, getRandomPosition, showPopup };
+})();
