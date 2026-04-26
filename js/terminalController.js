62d12aa25ab6e83cc38464298fab8e7b4d29dcd9
Catherine Jason <catherine.wunder89@icloud.com>
2026-04-26 17:31:20 -0400

    Update terminalController.js

--- js/terminalController.js
+++ js/terminalController.js
@@ -3,7 +3,7 @@ App.TerminalController = (function() {
     function onDeviceSelected(id) {
         if (id) {
             const dev = App.Engine.getDevices().find(d => d.id === id);
-            if (dev && (dev.type === 'router' || dev.type === 'switch' || dev.type === 'pc')) {
+            if (dev && (dev.type === 'router' || dev.type === 'switch' || dev.type === 'pc' || dev.type === 'firewall' || dev.type === 'server')) {
                 App.TerminalUI.show(id);
             } else App.TerminalUI.hide();
         } else App.TerminalUI.hide();
